b93eb683f37771ef1b12c4e201145256125455ee
Henry Morgan Stewart <henry@Henrys-MacBook-Pro-2.local>
2025-10-02 23:11:24 +0100

    init

---
+++ sql_server/NSCLC/4_NSCLC_EXCLUDED_CONCEPTS_COUNTS.sql
@@ -0,0 +1,55 @@
+WITH 
+mets AS (
+  SELECT person_id, min(measurement_date) as first_measurement_date 
+  FROM @cdm_database_schema.measurement m
+  INNER JOIN #Codesets cs 
+    ON m.measurement_concept_id = cs.concept_id
+  WHERE cs.codeset_id = 6
+  GROUP BY person_id
+),
+diags AS (
+  SELECT person_id, condition_start_date 
+  FROM @cdm_database_schema.condition_occurrence co
+  INNER JOIN #Codesets cs 
+    ON co.condition_concept_id = cs.concept_id
+  WHERE cs.codeset_id = 12
+),
+excluded_concepts AS (
+  SELECT cs.concept_id 
+  FROM #Codesets cs
+  LEFT JOIN #Codesets cs2 
+    ON cs.concept_id = cs2.concept_id
+    AND cs2.codeset_id = 12
+  WHERE cs.codeset_id = 13
+  AND cs2.concept_id is null
+),
+combined_population AS (
+  SELECT DISTINCT 
+    mets.person_id, 
+    mets.first_measurement_date, 
+    diags.condition_start_date, 
+    DATEDIFF(day, mets.first_measurement_date, diags.condition_start_date) as days_to_diag,
+    ROW_NUMBER() OVER (PARTITION BY mets.person_id ORDER BY ABS(DATEDIFF(day, mets.first_measurement_date, diags.condition_start_date)), DATEDIFF(day, mets.first_measurement_date, diags.condition_start_date)) as rn  
+  FROM mets
+  INNER JOIN diags 
+    ON mets.person_id = diags.person_id
+),
+diags_to_exclude AS (
+  SELECT person_id, condition_concept_id,
+         min(condition_start_date) as condition_start_date,
+         count(*) as n_conditions
+  FROM @cdm_database_schema.condition_occurrence co
+  INNER JOIN excluded_concepts cs 
+    ON co.condition_concept_id = cs.concept_id
+  WHERE co.person_id not in (select person_id from combined_population)
+  GROUP BY person_id, condition_concept_id
+)
+SELECT c.concept_name, 
+       COUNT(DISTINCT d.person_id) as excluded_patient_count,
+       COUNT(DISTINCT case when d.n_conditions > 1 then d.person_id end) as excluded_patient_count_multiple,
+       COUNT(DISTINCT case when d.n_conditions > 2 then d.person_id end) as excluded_patient_count_multiple_2,
+       COUNT(DISTINCT case when d.n_conditions > 3 then d.person_id end) as excluded_patient_count_multiple_3
+FROM diags_to_exclude d
+JOIN   @cdm_database_schema.concept c on c.concept_id = d.condition_concept_id
+GROUP BY d.condition_concept_id, c.concept_name
+ORDER BY excluded_patient_count DESC;
---
+++ sql_server/NSCLC/6_NSCLC_TIME_TO_TREATMENT_CLEAN_COHORT.sql
@@ -0,0 +1,103 @@
+WITH 
+mets AS (
+  SELECT person_id, min(measurement_date) as first_measurement_date 
+  FROM @cdm_database_schema.measurement m
+  INNER JOIN #Codesets cs 
+    ON m.measurement_concept_id = cs.concept_id
+  WHERE cs.codeset_id = 6
+  GROUP BY person_id
+),
+diags AS (
+  SELECT person_id, condition_start_date 
+  FROM @cdm_database_schema.condition_occurrence co
+  INNER JOIN #Codesets cs 
+    ON co.condition_concept_id = cs.concept_id
+  WHERE cs.codeset_id = 12
+),
+excluded_concepts AS (
+  SELECT cs.concept_id 
+  FROM #Codesets cs
+  LEFT JOIN #Codesets cs2 
+    ON cs.concept_id = cs2.concept_id
+    AND cs2.codeset_id = 12
+  WHERE cs.codeset_id = 13
+  AND cs2.concept_id is null
+),
+diags_to_exclude AS (
+  SELECT person_id, condition_concept_id,condition_start_date 
+  FROM @cdm_database_schema.condition_occurrence co
+  INNER JOIN excluded_concepts cs 
+    ON co.condition_concept_id = cs.concept_id
+),
+l01_exposures AS (
+  SELECT person_id, drug_exposure_start_date 
+  FROM @cdm_database_schema.drug_exposure de
+  INNER JOIN #Codesets cs 
+    ON de.drug_concept_id = cs.concept_id
+  WHERE cs.codeset_id = 11
+),
+combined_population_treated AS (
+  SELECT DISTINCT 
+    mets.person_id, 
+    DATEDIFF(day, min(mets.first_measurement_date), min(de.drug_exposure_start_date)) as days_to_treatment  
+   FROM mets
+  INNER JOIN l01_exposures de
+    ON mets.person_id = de.person_id
+    AND de.drug_exposure_start_date >= mets.first_measurement_date
+  WHERE mets.person_id not in (select person_id from diags_to_exclude)
+  AND mets.person_id in (select person_id from diags)
+  GROUP BY mets.person_id
+),
+combined_population_treated_ranked AS (
+  SELECT DISTINCT 
+    person_id, 
+    days_to_treatment,
+    row_number() over (order by days_to_treatment) as order_nr
+   FROM combined_population_treated
+),
+combined_population_treated_n AS (
+  SELECT COUNT(DISTINCT person_id) as n FROM combined_population_treated_ranked
+)
+SELECT 
+  n,
+  MIN(days_to_treatment) as min_days_to_treatment,
+   MIN(CASE
+            WHEN order_nr < .05 * n
+                THEN 9999
+            ELSE days_to_treatment
+            END) AS q5_days_to_diag,
+ MIN(CASE
+            WHEN order_nr < .10 * n
+                THEN 9999
+            ELSE days_to_treatment
+            END) AS q10_days_to_diag,
+     MIN(CASE
+            WHEN order_nr < .25 * n
+                THEN 9999
+            ELSE days_to_treatment
+            END) AS q25_days_to_diag,
+    MIN(CASE
+            WHEN order_nr < .50 * n
+                THEN 9999
+            ELSE days_to_treatment
+            END) AS median_days_to_diag,
+    MIN(CASE
+            WHEN order_nr < .75 * n
+                THEN 9999
+            ELSE days_to_treatment
+            END) AS q75_days_to_diag,
+    MIN(CASE
+            WHEN order_nr < .9 * n
+                THEN 9999
+            ELSE days_to_treatment
+            END) AS q90_days_to_diag,
+        MIN(CASE
+            WHEN order_nr < .95 * n
+                THEN 9999
+            ELSE days_to_treatment
+            END) AS q95_days_to_diag,
+    MAX(days_to_treatment) AS max_days_to_treatment
+FROM combined_population_treated_ranked
+JOIN combined_population_treated_n n
+    ON 1 = 1
+GROUP BY n;
